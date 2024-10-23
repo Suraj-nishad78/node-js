eeb2805b01d13d7fdbdcaf526343793ed5ab7f0b
Suraj-nishad78 <iamsuraj0737@gmail.com>
2024-10-23 18:53:43 +0530

    Update

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const http = require('http')
 const fs = require('fs')
+const readline = require('readline')
+const { stdin, stdout } = require('process')
 
-const htmlData = fs.readFileSync('index.html')
-const server = http.createServer((req, res)=>{
-    res.end(htmlData)
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
 })
+let num1;
+let num2;
+ rl.question('Enter the first number: ',(numx)=>{
+    num1 = Number(numx);
+    rl.question('Enter the second number: ',(numy)=>{
+        num2 = Number(numy);
+        if(num1 > num2){
+            console.log(`the maximum of two number is: ${num1}`)
+        }else if(num1 < num2){
+            console.log(`the maximum of two number is: ${num2}`)
+        } else{
+            console.log('Invalid Number')
+        }
+        rl.close();
+    })
+ })
 
-server.listen(3000,()=>{
-    console.log('Server running on port 3000')
-})
+
+
